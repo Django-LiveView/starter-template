256ec3c1eb38d9459e9f2740b4c17d4c5af5e836
Andros Fenollosa <tanrax@users.noreply.github.com>
2023-02-17 15:59:32 +0100

    Update django-launcher.sh

--- django-launcher.sh
+++ django-launcher.sh
@@ -3,6 +3,10 @@
 # Make .mo files for translations
 python3 manage.py compilemessages
 
+
+# Make migrations
+python3 manage.py migrate
+
 # Start server with debug mode
 python3 manage.py runserver 0.0.0.0:8000
 # Start server with production mode
